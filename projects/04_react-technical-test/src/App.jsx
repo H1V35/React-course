8852fe7b534fafdb24eb5653b325c85fcf5de399
H1V35 <higesjorge@gmail.com>
2024-01-29 21:25:28 +0100

    ✅ done: Base part of technical test completed

--- projects/04_react-technical-test/src/App.jsx
+++ projects/04_react-technical-test/src/App.jsx
@@ -1,32 +1,51 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useEffect, useState } from "react";
+
+const CAT_ENDOPOINT_RANDOM_FACT = "https://catfact.ninja/fact";
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
+
+  useEffect(() => {
+    fetch(CAT_ENDOPOINT_RANDOM_FACT)
+      .then((res) => res.json())
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (!fact) return;
+
+    const threeFirstWords = fact.split(" ", 3).join(" ");
+    const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${threeFirstWords}?fontSize=50&fontColor=white&json=true`;
+
+    fetch(CAT_ENDPOINT_IMAGE_URL)
+      .then((res) => res.json())
+      .then((response) => {
+        const { _id } = response;
+        const url = `https://cataas.com/cat/${_id}/says/${threeFirstWords}?fontSize=50&fontColor=white`;
+
+        setImageUrl(url);
+      });
+  }, [fact]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="flex flex-col items-center align-middle bg-zinc-900 rounded-3xl w-[800px]">
+      <h1 className="text-5xl font-extrabold px-12 pt-12 pb-8">Gatetes App</h1>
+      {imageUrl && (
+        <picture className="px-12 pb-8">
+          <img
+            src={imageUrl}
+            alt={`Image extracted using the first three words for ${fact}`}
+            className="max-h-[600px] rounded-3xl"
+          />
+        </picture>
+      )}
+      {fact && (
+        <p className="text-center text-2xl font-medium px-12 pb-12">{fact}</p>
+      )}
+    </main>
   );
 }
