f442f7434c10c039895eaac3cba8d835c95a8c49
H1V35 <higesjorge@gmail.com>
2024-01-30 20:07:36 +0100

    🎉 done: Project 04_react-technical-test completed

--- projects/04_react-technical-test/src/App.jsx
+++ projects/04_react-technical-test/src/App.jsx
@@ -1,12 +1,22 @@
 import { useCatFact } from "./hooks/useCatFact";
 import { useCatImage } from "./hooks/useCatImage";
 
+const containerVariants = {
+  generic:
+    "w-[850px] h-[850px] bg-zinc-900 rounded-3xl flex flex-col items-center justify-between gap-4 align-middle",
+};
+
+const buttonVariants = {
+  generic:
+    "px-6 py-2 rounded-full border border-[#ffffffde] font-bold text-base transition duration-300 hover:bg-[#ffffffde] hover:text-zinc-900",
+};
+
 export function App() {
   const { fact, refreshFact } = useCatFact();
   const { imageUrl } = useCatImage({ fact });
 
   return (
-    <main className="w-[850px] h-[850px] flex flex-col justify-between items-center gap-4 align-middle bg-zinc-900 rounded-3xl">
+    <main className={`${containerVariants.generic}`}>
       <h1 className="text-5xl font-extrabold mt-12">Gatetes App</h1>
 
       {imageUrl && (
@@ -22,10 +32,7 @@ export function App() {
       <section className="flex flex-col justify-between items-center gap-6 px-12 pb-12">
         {fact && <p className="text-center text-2xl font-medium">{fact}</p>}
 
-        <button
-          onClick={refreshFact}
-          className="font-bold text-base px-6 py-2 rounded-full border border-[#ffffffde] transition duration-300 hover:text-zinc-900 hover:bg-[#ffffffde]"
-        >
+        <button onClick={refreshFact} className={`${buttonVariants.generic}`}>
           Get new fact
         </button>
       </section>
