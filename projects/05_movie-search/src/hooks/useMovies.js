a440e501a4a0621767053b9c934ab5fcc6f85617
H1V35 <higesjorge@gmail.com>
2024-02-05 18:36:38 +0100

    ✨ feat: Add functionality to prevent redundant movie searches using React.useRef()

--- projects/05_movie-search/src/hooks/useMovies.js
+++ projects/05_movie-search/src/hooks/useMovies.js
@@ -5,11 +5,15 @@ export function useMovies({ search }) {
   const [movies, setMovies] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState(null);
+  const previousSearch = React.useRef(search);
 
   const getMovies = async () => {
+    if (search === previousSearch.current) return;
+
     try {
       setLoading(true);
       setError(null);
+      previousSearch.current = search;
       const newMovies = await searchMovies({ search });
       setMovies(newMovies);
     } catch (e) {
