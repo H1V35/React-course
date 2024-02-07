52cea13d331c41f801e8acc560c73b4ee6d6eb6f
H1V35 <higesjorge@gmail.com>
2024-02-07 20:40:42 +0100

    ✨ feat: Implement single-page application (SPA) functionality without dependencies

--- projects/06_react-router/src/App.jsx
+++ projects/06_react-router/src/App.jsx
@@ -1,32 +1,81 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import avatar from "./assets/images/avatar.jpg";
+
+const NAVIGATION_EVENT = "pushstate";
+
+function navigate(href) {
+  window.history.pushState({}, "", href);
+  const navigationEvent = new Event(NAVIGATION_EVENT);
+  window.dispatchEvent(navigationEvent);
+}
+
+function HomePage() {
+  return (
+    <section className="w-full flex flex-col gap-8">
+      <h1 className="text-5xl font-extrabold">Home</h1>
+
+      <p className="text-xl">
+        This is an example page to create React Router from scratch
+      </p>
+
+      {/* <a href="/about" className="font-bold text-fuchsia-500">
+        Go to About Us
+      </a> */}
+      <button
+        onClick={() => navigate("/about")}
+        className="w-52 px-6 py-2 bg-purple-600 rounded-full border-2 border-purple-600 font-bold transition duration-300 hover:bg-purple-500 hover:border-[#ffffffdd]"
+      >
+        Go to About Us
+      </button>
+    </section>
+  );
+}
+
+function AboutPage() {
+  return (
+    <section className="w-full flex flex-col gap-8">
+      <h1 className="text-5xl font-extrabold">About</h1>
+
+      <article className="flex gap-6 items-end">
+        <img src={avatar} alt="Hive's Avatar" className="w-64 rounded-3xl" />
+
+        <p className="text-xl">
+          Hi, my name is Hive and I&#39;m creating a clone of React Router
+        </p>
+      </article>
+
+      {/* <a href="/" className="font-bold text-fuchsia-500">
+        Go to Home
+      </a> */}
+      <button
+        onClick={() => navigate("/")}
+        className="w-52 px-6 py-2 bg-purple-600 rounded-full border-2 border-purple-600 font-bold transition duration-300 hover:bg-purple-500 hover:border-[#ffffffdd]"
+      >
+        Go to Home
+      </button>
+    </section>
+  );
+}
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const onLocationChange = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+
+    return () => {
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="w-[600px] flex flex-col items-center gap-8">
+      {currentPath === "/" && <HomePage />}
+      {currentPath === "/about" && <AboutPage />}
+    </main>
   );
 }
